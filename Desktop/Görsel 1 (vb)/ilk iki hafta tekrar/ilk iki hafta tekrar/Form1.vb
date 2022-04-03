cb4b367eb272f10f283624a4d698bb74cf2e14a8
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-03 10:13:38 +0300

    gorsel programlama

---
+++ Desktop/Görsel 1 (vb)/ilk iki hafta tekrar/ilk iki hafta tekrar/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+
+   
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Say1, say2, Sonuc As Double
+        Say1 = TextBox1.Text
+        say2 = TextBox2.Text
+        Sonuc = Say1 ^ say2
+        Label7.Text = Sonuc
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim Say1, say2, Sonuc
+        Say1 = TextBox1.Text
+        say2 = TextBox2.Text
+        Sonuc = Say1 + " " + say2
+        Label7.Text = Sonuc
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ListBox1.Items.Add("manuel ekleme")
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        ListBox1.Items.Add(TextBox3.Text)
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click, Label3.Click
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Dim eklenen
+        eklenen = TextBox3.Text
+        ListBox1.Items.Add(TextBox3.Text)
+    End Sub
+    
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Dim eklenensayı As Integer
+        eklenensayı = TextBox3.Text
+        ListBox1.Items.Add(TextBox3.Text.ToString)
+    End Sub
+
+    Private Sub Label6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label6.Click, Label10.Click
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label8.Click, Label9.Click
+
+    End Sub
+
+    Private Sub Label7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        Dim silinecekdeğer As String
+        silinecekdeğer = TextBox4.Text
+        ListBox1.Items.Remove(silinecekdeğer)
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        Dim indeksnumarası As Integer
+        ListBox1.Items.RemoveAt(indeksnumarası)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim seçilinesne As Integer
+        seçilinesne = ListBox1.SelectedIndex
+        ListBox1.Items.RemoveAt(seçilinesne)
+    End Sub
+End Class
