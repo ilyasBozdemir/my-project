c77cd5237ffcdf5f6a722608f7fe601f62c05a35
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-03 13:49:09 +0300

    files added.

---
+++ App/proje/proje eski hali/Stok_Takip/Stok_Takip/Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stok_Takip
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stok_Bilg stok =new Stok_Bilg();
+            stok.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Urun_satıs urun = new Urun_satıs();
+            urun.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Personel person = new Personel();
+            person.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Musteri musteri = new Musteri();
+            musteri.ShowDialog();
+        }
+    }
+}
