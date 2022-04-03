ed6bc30c78c1d2875b6039373cc4b6d5bb8d74cc
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-03 13:36:53 +0300

    files added.

---
+++ Görsel Programlama II (vb)/restoran ornek/ders1/ders1/Form1.Designer.vb
@@ -0,0 +1,429 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox3 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox4 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox5 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox6 = New System.Windows.Forms.CheckedListBox()
+        Me.CheckedListBox7 = New System.Windows.Forms.CheckedListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"Mercimek Çorbası", "Ezo gelin Çorbası", "Arabaşı Çorbası", "Tavuk Çorbası", "Yayla Çorbası", "Bamya Çorbası", "Şehriye Çorbası", "Domates Çorbası", "Tarhana Çorbası"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(28, 57)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(145, 139)
+        Me.CheckedListBox1.TabIndex = 0
+        '
+        'CheckedListBox2
+        '
+        Me.CheckedListBox2.FormattingEnabled = True
+        Me.CheckedListBox2.Items.AddRange(New Object() {"Çoban Salatası", "Soğan Salatası", "Rus Salatası", "Patates Salatası", "Semiz otu Salatası", "Yoğurt", "Cacık", "Piyaz", "Patlıcan Salatası", "Soğuş", "Ezme", "Tarator", "Babaganduş", "Gavurdağı Salatası"})
+        Me.CheckedListBox2.Location = New System.Drawing.Point(179, 57)
+        Me.CheckedListBox2.Name = "CheckedListBox2"
+        Me.CheckedListBox2.Size = New System.Drawing.Size(157, 139)
+        Me.CheckedListBox2.TabIndex = 1
+        '
+        'CheckedListBox3
+        '
+        Me.CheckedListBox3.FormattingEnabled = True
+        Me.CheckedListBox3.Items.AddRange(New Object() {"Karnıyanık", "Kuru fasulye", "Pirinç Pilavı", "Bulgur Pilavı", "İçli Köfte", "Mantı", "Makarna Çeşitleri", "Taze fasulye", "Musakka", "Şakşuka", "Dolma", "Börek", "Kıymalı Börek", "Peynirli Börek"})
+        Me.CheckedListBox3.Location = New System.Drawing.Point(342, 57)
+        Me.CheckedListBox3.Name = "CheckedListBox3"
+        Me.CheckedListBox3.Size = New System.Drawing.Size(165, 139)
+        Me.CheckedListBox3.TabIndex = 2
+        '
+        'CheckedListBox4
+        '
+        Me.CheckedListBox4.FormattingEnabled = True
+        Me.CheckedListBox4.Items.AddRange(New Object() {"Şiş Köfte", "Tandır Kebap", "Konya Tandır Kebap", "Kuzu Şiş", "Adana ", "Urfa", "Et iskender", "Tavuk iskender", "Beyti Kebap", "Kuzu Pirzola ızgara", "Tavuk şiş", "Kızarmış tavuk", "Tantuni", "Ciğer kebap", "Köfte", "Sucuk"})
+        Me.CheckedListBox4.Location = New System.Drawing.Point(513, 57)
+        Me.CheckedListBox4.Name = "CheckedListBox4"
+        Me.CheckedListBox4.Size = New System.Drawing.Size(157, 139)
+        Me.CheckedListBox4.TabIndex = 3
+        '
+        'CheckedListBox5
+        '
+        Me.CheckedListBox5.FormattingEnabled = True
+        Me.CheckedListBox5.Items.AddRange(New Object() {"Pizza-Karışık", "Pizza-İtalya", "Pizza-Türk", "Pizza-Tonno", "Pizza-Vejeteryan", "Hamburger menu", "Hamburger KNG Menu", "Hamburger SALTBAE Menu", "Etli ekmek", "Bıçak arası"})
+        Me.CheckedListBox5.Location = New System.Drawing.Point(676, 57)
+        Me.CheckedListBox5.Name = "CheckedListBox5"
+        Me.CheckedListBox5.Size = New System.Drawing.Size(196, 139)
+        Me.CheckedListBox5.TabIndex = 4
+        '
+        'CheckedListBox6
+        '
+        Me.CheckedListBox6.FormattingEnabled = True
+        Me.CheckedListBox6.Items.AddRange(New Object() {"Künefe", "Revani", "Cheese Cake", "Antep Sarması", "Baklava Çeşitleri", "Cezerye", "Sütlaç", "Aşure", "Kazandibi", "Tavuk Göğsü", "Kemal Paşa Tatlısı", "Dondurma", "Halka Tatlısı", "Kadayıf", "Fıstıklı sarma", "Tulumba", "Tiramisu", "Havuçlu kek", "Yaş Pasta Çeşitleri"})
+        Me.CheckedListBox6.Location = New System.Drawing.Point(22, 283)
+        Me.CheckedListBox6.Name = "CheckedListBox6"
+        Me.CheckedListBox6.Size = New System.Drawing.Size(159, 154)
+        Me.CheckedListBox6.TabIndex = 5
+        '
+        'CheckedListBox7
+        '
+        Me.CheckedListBox7.FormattingEnabled = True
+        Me.CheckedListBox7.Items.AddRange(New Object() {"Kola", "Ayran", "Su", "Şalgam", "Fanta", "Gazoz", "Meyve Suyu", "Soğuk Çay", "Soda-Sade", "Soda-Meyveli", "Limonata", "Şerbet Çeşitleri", "Komposto", "Enerji İçeçeği"})
+        Me.CheckedListBox7.Location = New System.Drawing.Point(187, 283)
+        Me.CheckedListBox7.Name = "CheckedListBox7"
+        Me.CheckedListBox7.Size = New System.Drawing.Size(149, 154)
+        Me.CheckedListBox7.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Çorba Menüsü"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(210, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Salata Menüsü"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(359, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Ev Yemeği Menüsü"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(527, 41)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Et Yemeği Menüsü"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(710, 41)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Hızlı Menüler"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(46, 267)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(68, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Tatlı Menüsü"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(202, 267)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(81, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "İçecek Menüsü"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Tam", "Az", "Tereyağlı", "Tuzlu", "Az Tuzlu", "Kaşar İlave", "Kıtır İlave"})
+        Me.ComboBox1.Location = New System.Drawing.Point(28, 202)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(142, 21)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(182, 203)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(153, 20)
+        Me.TextBox1.TabIndex = 15
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(342, 202)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(165, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(513, 203)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(157, 20)
+        Me.TextBox3.TabIndex = 17
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(676, 203)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(196, 20)
+        Me.TextBox4.TabIndex = 18
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(22, 443)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(159, 20)
+        Me.TextBox5.TabIndex = 19
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(187, 443)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(149, 20)
+        Me.TextBox6.TabIndex = 20
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(48, 229)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 22)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "Ekle"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(204, 229)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 22)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "Ekle"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(362, 228)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 22)
+        Me.Button3.TabIndex = 23
+        Me.Button3.Text = "Ekle"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(530, 228)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 22)
+        Me.Button4.TabIndex = 24
+        Me.Button4.Text = "Ekle"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(713, 228)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(100, 22)
+        Me.Button5.TabIndex = 25
+        Me.Button5.Text = "Ekle"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(49, 469)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(100, 22)
+        Me.Button6.TabIndex = 26
+        Me.Button6.Text = "Ekle"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(205, 469)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 22)
+        Me.Button7.TabIndex = 27
+        Me.Button7.Text = "Ekle"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.DataGridView1.Location = New System.Drawing.Point(351, 310)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(443, 153)
+        Me.DataGridView1.TabIndex = 28
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Sipariş ID"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Sipariş"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Sipariş Özelliği"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Masa NO"
+        Me.Column4.Name = "Column4"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "78", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35"})
+        Me.ComboBox2.Location = New System.Drawing.Point(434, 283)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 29
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(359, 286)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(50, 13)
+        Me.Label8.TabIndex = 30
+        Me.Label8.Text = "Masa No"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(962, 562)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckedListBox7)
+        Me.Controls.Add(Me.CheckedListBox6)
+        Me.Controls.Add(Me.CheckedListBox5)
+        Me.Controls.Add(Me.CheckedListBox4)
+        Me.Controls.Add(Me.CheckedListBox3)
+        Me.Controls.Add(Me.CheckedListBox2)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents CheckedListBox2 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents CheckedListBox3 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents CheckedListBox4 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents CheckedListBox5 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents CheckedListBox6 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents CheckedListBox7 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ Görsel Programlama II (vb)/restoran ornek/ders1/ders1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
