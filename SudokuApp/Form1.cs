cae390da4a8ebdda9ca1a9f55e89a98ec766682d
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-03 09:55:12 +0300

    sudoku App

---
+++ SudokuApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //listBox1.Items.Clear();
+            //List<int> randomNumbers = new List<int>();
+            //Random random = new Random();
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    int number;
+            //    do number = random.Next(1, 10);
+            //    while (randomNumbers.Contains(number));
+            //    randomNumbers.Add(number);
+            //}
+
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    listBox1.Items.Add(randomNumbers[i]);
+            //}
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //to be continued
+        }
+    }
+}
