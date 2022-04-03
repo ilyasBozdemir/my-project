38a6ba1f94d14d409245c2277b5cf094bafa6b6a
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-03 10:18:39 +0300

    diziler ve random sinifi kullanimi

---
+++ Desktop/Yazılım Mimarileri/diziler ve random sinifi/Backup/csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] örnekler = new int[20];
+            Random random = new Random();
+            int rastgele;
+            int sayac = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                random.Next(0, 200);
+                rastgele[sayac] = random[i];
+            }
+            Console.ReadKey();
+        } 
+    }
+}
