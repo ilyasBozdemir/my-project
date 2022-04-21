2bc786464f0bb5098ab7725045d2c7b39bbcd945
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-21 15:38:13 +0300

    week arrays updated

--- Algoritma Soruları ve Cevaplari/Algoritma-Sorulari/Diziler_1/Program.cs
+++ Algoritma Soruları ve Cevaplari/Algoritma-Sorulari/Diziler_1/Program.cs
@@ -1,1 +1,189 @@
-﻿
+﻿//Cevap1a();
+//Cevap1b();
+//Cevap1c();
+//Cevap1d();
+//Cevap1e();
+//Cevap2();
+//Cevap3();
+//Cevap4();
+//Cevap5();
+//Cevap6();
+//Cevap7();
+//Cevap8();
+//Cevap9();
+//Cevap10a();
+//Cevap10b();
+//Cevap10c();
+//Cevap10d();
+//Cevap10e();
+//Cevap10f();
+//Cevap10g();
+//Cevap11();
+//Cevap12();
+//Cevap13();
+//Cevap14();
+//Cevap15();
+//Cevap16();
+//Cevap17();
+//Cevap18();
+//Cevap19();
+//Cevap20a();
+//Cevap20b();
+//Cevap20c();
+//Cevap20d();
+//Cevap20e();
+//Cevap20f();
+//Cevap20g();
+//Cevap20h();
+
+static void Cevap1a()
+{
+
+}
+static void Cevap1b()
+{
+
+}
+static void Cevap1c()
+{
+
+}
+static void Cevap1d()
+{
+
+}
+static void Cevap1e()
+{
+
+}
+static void Cevap2()
+{
+
+}
+static void Cevap3()
+{
+
+}
+static void Cevap4()
+{
+
+}
+static void Cevap5()
+{
+
+}
+static void Cevap6()
+{
+
+}
+static void Cevap7()
+{
+
+}
+static void Cevap8()
+{
+
+}
+static void Cevap9()
+{
+
+}
+static void Cevap10a()
+{
+
+}
+static void Cevap10b()
+{
+
+}
+static void Cevap10c()
+{
+
+
+}
+static void Cevap10d()
+{
+
+}
+static void Cevap10e()
+{
+
+}
+static void Cevap10f()
+{
+
+}
+static void Cevap10g()
+{
+
+}
+static void Cevap11()
+{
+
+}
+static void Cevap12()
+{
+
+}
+static void Cevap13()
+{
+
+}
+static void Cevap14()
+{
+
+}
+static void Cevap15()
+{
+
+}
+static void Cevap16()
+{
+
+}
+static void Cevap17()
+{
+
+
+}
+static void Cevap18()
+{
+
+}
+static void Cevap19()
+{
+
+}
+static void Cevap20a()
+{
+
+}
+static void Cevap20b()
+{
+
+}
+static void Cevap20c()
+{
+
+}
+static void Cevap20d()
+{
+
+}
+static void Cevap20e()
+{
+
+}
+static void Cevap20f()
+{
+
+}
+static void Cevap20g()
+{
+
+}
+static void Cevap20h()
+{
+
+}
+
