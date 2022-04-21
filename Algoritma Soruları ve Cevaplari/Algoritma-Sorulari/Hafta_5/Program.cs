0a78c60cb3545ed30ce18646d2816d39c45ea466
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-21 15:31:42 +0300

    week 5 updated

--- Algoritma Soruları ve Cevaplari/Algoritma-Sorulari/Hafta_5/Program.cs
+++ Algoritma Soruları ve Cevaplari/Algoritma-Sorulari/Hafta_5/Program.cs
@@ -1,1 +1,65 @@
-﻿
+﻿//Cevap1();
+//Cevap2();
+//Cevap3();
+//Cevap4();
+//Cevap5();
+//Cevap6();
+//Cevap7();
+//Cevap8();
+//Cevap9();
+//Cevap10();
+//Cevap11();
+//Cevap12();
+//Cevap13();
+static void Cevap1()
+{
+
+}
+static void Cevap2()
+{
+
+}
+static void Cevap3()
+{
+
+}
+static void Cevap4()
+{
+
+}
+static void Cevap5()
+{
+
+}
+static void Cevap6()
+{
+
+}
+static void Cevap7()
+{
+
+}
+static void Cevap8()
+{
+
+}
+static void Cevap9()
+{
+
+}
+static void Cevap10()
+{
+
+}
+static void Cevap11()
+{
+
+}
+static void Cevap12()
+{
+
+}
+static void Cevap13()
+{
+
+}
