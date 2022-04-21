592f0ee90ac76725fab09e96d1ea1da3b2cb045e
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-21 15:32:32 +0300

    week 6 updated

--- Algoritma Soruları ve Cevaplari/Algoritma-Sorulari/Hafta_6/Program.cs
+++ Algoritma Soruları ve Cevaplari/Algoritma-Sorulari/Hafta_6/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿//Cevap1();
+//Cevap2();
+//Cevap3();
+//Cevap4();
+//Cevap5();
+//Cevap6();
+//Cevap7();
+//Cevap8();
+//Cevap9();
+//Cevap10();
+static void Cevap1()
+{
+
+}
+static void Cevap2()
+{
+
+}
+static void Cevap3()
+{
+
+}
+static void Cevap4()
+{
+
+}
+static void Cevap5()
+{
+
+}
+static void Cevap6()
+{
+
+}
+static void Cevap7()
+{
+
+}
+static void Cevap8()
+{
+
+}
+static void Cevap9()
+{
+
+}
+static void Cevap10()
+{
+
+}
