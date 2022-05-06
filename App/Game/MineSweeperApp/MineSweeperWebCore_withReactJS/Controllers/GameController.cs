6e82906d69c32b266e964b906114ce3e9abb6898
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-05-06 17:32:53 +0300

    .

---
+++ App/Game/MineSweeperApp/MineSweeperWebCore_withReactJS/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MineSweeperWebCore_withReactJS.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
