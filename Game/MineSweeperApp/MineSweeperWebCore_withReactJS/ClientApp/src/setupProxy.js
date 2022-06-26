406ef13e9174e6206d0cf24b22eb25307ba7bc68
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-06-26 22:09:44 +0300

    MineSweeperWebCore_withReactJS added

--- Game/MineSweeperApp/MineSweeperWebCore_withReactJS/ClientApp/src/setupProxy.js
+++ Game/MineSweeperApp/MineSweeperWebCore_withReactJS/ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:8980';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:62474';
 
 const context =  [
   "/weatherforecast",
